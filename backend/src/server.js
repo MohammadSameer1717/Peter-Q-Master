7a95a35b3b665d3b07d47f1edd4dc9dc1f8ef14c
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-12-28 22:31:03 +0530

    PTR-01

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-import { serve } from "inngest/express";
+import { serve} from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
-
-import { ENV } from "./lib/env.js";
+ 
+ 
+import { ENV } from "./lib/env.js"; 
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
-
+ 
+ 
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoute.js";
 
